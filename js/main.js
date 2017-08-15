84e7eb4fed12c203dfb8ded0903cb74eaef2c289
Liang Xu <1040664047@qq.com>
2017-08-15 15:39:29 +0800

    Add preLoader/scrollTop/scrollSpy

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 (function ($) {
+    'use strict';
+    //Preloader
+    $(window).load(function () {
+        $('.preloader').delay(1000).fadeOut('slow');
+    });
     //Sticky Header
     $(document).ready(function () {
         $(window).scroll(function () {
@@ -8,22 +13,44 @@
             else {
                 $('body').removeClass('appear-nav')
             }
-        })
+        });
+        $(window).scroll();
+        // ScrollSpy
+        $("body").scrollspy({
+            target  : '.navbar',
+            offset  : 65
+        });
+
+        //Scroll Top
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 60
+                    }, 500);
+                    return false;
+                }
+            }
+        });
+
+        //Slide Background
+        $('.intro').vegas({
+            slides:[
+                {src: "images/banner/slide_img01.jpg"},
+                {src:"images/banner/slide_img02.jpg"}
+            ]
+        });
+        //Scroll Animation
+        var wow = new WOW(
+            {
+                mobile:false
+            }
+        );
+        wow.init();
     });
-    //Slide Background
-    $('.intro').vegas({
-        slides:[
-            {src: "images/banner/slide_img01.jpg"},
-            {src:"images/banner/slide_img02.jpg"}
-        ]
-    });
-    //Scroll Animation
-    var wow = new WOW(
-        {
-            mobile:false
-        }
-    );
-    wow.init();
+
     //Feedback Carousel
     $('.feedback-carousel').owlCarousel({
         loop: true,
